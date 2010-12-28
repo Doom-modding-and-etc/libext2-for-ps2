9d9b1dabc8e4813b5922da8e0aca0f586b962c16
dimok321 <dimok321@hotmail.de>
2010-12-28 10:59:20 +0000

    *Sync with git snapshot 2010-12-24 *Removed unused e2p source

--- include/ext2.h
+++ include/ext2.h
@@ -1,22 +1,22 @@
- /**
- * ext2.h - devoptab file routines for EXT2/3/4-based devices.
- *
- * Copyright (c) 2010 Dimok
- *
- * This program/include file is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as published
- * by the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program/include file is distributed in the hope that it will be
- * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
+/********************************************************************************
+ * ext2.h - devoptab file routines for EXT2/3/4-based devices.                  *
+ *                                                                              *
+ * Copyright (c) 2010 Dimok                                                     *
+ *                                                                              *
+ * This program/include file is free software; you can redistribute it and/or   *
+ * modify it under the terms of the GNU General Public License as published     *
+ * by the Free Software Foundation; either version 2 of the License, or         *
+ * (at your option) any later version.                                          *
+ *                                                                              *
+ * This program/include file is distributed in the hope that it will be         *
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty          *
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ * GNU General Public License for more details.                                 *
+ *                                                                              *
+ * You should have received a copy of the GNU General Public License            *
+ * along with this program; if not, write to the Free Software Foundation,      *
+ * Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA                *
+ ********************************************************************************/
 #ifndef __EXT2_H_
 #define __EXT2_H_
 
